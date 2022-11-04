4c634a290ed4547232ba80bfc16900c4e4ffdead
Viktor Kostadinov <viktor.pts@abv.bg>
2022-11-04 21:52:23 +0200

    Project files

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { checkUserNav } from './auth.js';
+import { showCatalogView } from './catalog.js';
+import './details.js';
+import './login.js';
+
+
+checkUserNav();
+
+// Start application in catalog view
+showCatalogView();
---
+++ src/details.js
@@ -0,0 +1,40 @@
+// get information about single recipe by id
+// display all information about recipe
+
+export async function showDetailsView(id) {
+    [...document.querySelectorAll('section')].forEach(s => s.style.display = 'none');
+    
+    const recipe = await getById(id);
+    
+    document.getElementById('details-view').style.display = 'block';
+
+    displayRecipe(recipe);
+}
+
+
+async function getById(id) {
+    const response = await fetch('http://localhost:3030/data/recipes/' + id);
+    const recipe = await response.json();
+
+    return recipe;
+}
+
+function displayRecipe(recipe) {
+    document.getElementById('recipe-name').textContent = recipe.name;
+
+    const ingredientsFragment = document.createDocumentFragment();
+    for (let item of recipe.ingredients) {
+        const element = document.createElement('li');
+        element.textContent = item;
+        ingredientsFragment.appendChild(element);
+    }
+    document.getElementById('recipe-ingredients').replaceChildren(ingredientsFragment);
+
+    const stepsFragment = document.createDocumentFragment();
+    for (let item of recipe.steps) {
+        const element = document.createElement('li');
+        element.textContent = item;
+        stepsFragment.appendChild(element);
+    }
+    document.getElementById('recipe-steps').replaceChildren(stepsFragment);
+}
